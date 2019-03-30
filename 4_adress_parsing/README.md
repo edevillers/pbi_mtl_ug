56ed0dc008c0987d92c869a276674ecf9949dfee
edevillers <edevillers@users.noreply.github.com>
2019-03-30 11:57:33 -0400

    ressusciter l'exemple du parsing d'adresse

--- 4_adress_parsing/README.md
+++ 4_adress_parsing/README.md
@@ -1,5 +1,8 @@
+## Récupérer les composants d'une adresse non-structurée. 
+
 On va utiliser une librairie qui s'appelle Pyap pour extraire les infos de notre chaîne de caractères. 
 La librairie est disponible sur [github](https://github.com/vladimarius/pyap) et peut aussi s'installer via pip : `pip install pyap`
 
 # TO_DO
-**Sur le backburner... à ressusciter si j'ai le temps de débugguer**
+**Pour les besoins de la démonstration, je supprime les rows où je n'ai pas de match.**
+**Une meilleure approche serait de retourner un match vide. Idéalement ça se ferait via un PR sur la librairie.**
--- 4_adress_parsing/test.py
+++ 4_adress_parsing/test.py
@@ -1,6 +1,41 @@
 import pyap
+import pandas as pd
+import json
 
 test_string = "C.W. Wiebe Medical Centre, 385 Main Street, Winkler, MB, R6W 1J2"
 
 parsed = pyap.parse(test_string, country='CA')
 print(parsed[0].as_dict())
+
+
+# data = {'adress': ["C.W. Wiebe Medical Centre, 385 Main Street, Winkler, MB, R6W 1J2"
+#                 ,"Tootelo, 204 boul Montarville, suite 2500, Boucherville, Qc, J4b6S2"]}
+# df = pd.DataFrame(data=data)
+# df.info()
+df = pd.read_csv('PhysicianDirectory.csv')
+
+print(df)
+
+df['concat'] = df['OFFICE_ADDRESS'] + ", " +  df['OFFICE_CITY'] + ", " + df['OFFICE_PROVINCE'] + ", " + df['OFFICE_POSTAL']
+
+# no_match = "{""full_address"": ""{full}"", ""full_street"": """", ""street_number"": """", ""street_name"": """", ""street_type"": """", ""route_id"": """", ""post_direction"": """", ""floor"": """", ""building_id"": """", ""occupancy"": """", ""postal_box"": """", ""city"": """", ""region1"": """", ""postal_code"": """", ""country_id"": """"}"
+
+def parse_adress(colonne, no_match=no_match):
+    try:
+        parsed_obj = pyap.parse(colonne, country='CA')
+        try:
+            parsed = parsed_obj[0].as_dict()
+        except IndexError:
+            parsed = ""
+    except TypeError:
+        print(colonne)
+        parsed = ""
+    parsed = json.dumps(parsed)
+    return parsed
+    
+
+df['parsed'] = df['concat'].apply(parse_adress)
+
+print(df['concat'])
+df.info()
+print(df)
